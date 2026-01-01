66f06d5282624f54fb1014a9963a0525ecdd60da
wh0ami-dev <alihaydarasgar@hotmail.com>
2026-01-01 17:22:36 +0300

    Chapter 1 and greybox scene objects fixing GameManager Script&Basics Script

--- Assets/Script/EthanCraneBasics.cs
+++ Assets/Script/EthanCraneBasics.cs
@@ -1,19 +1,20 @@
+using System.Collections;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class EthanCraneBasics : MonoBehaviour
 {
-    [Header("Movement Settings")]
     public float moveSpeed = 5f;
+    public bool canMove = true;
 
     private Rigidbody2D rb;
     private float moveInputX;
     private float moveInputY;
-
     private bool facingRight = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(WaitFlip());
     }
 
     void Update()
@@ -24,14 +25,10 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-       
+        if (!canMove) return;
         rb.linearVelocity = new Vector2(moveInputX * moveSpeed, moveInputY * moveSpeed);
-
-        // YÖN FLIP
-        if (moveInputX > 0 && !facingRight)
-            Flip();
-        else if (moveInputX < 0 && facingRight)
-            Flip();
+        if (moveInputX > 0 && !facingRight) Flip();
+        else if (moveInputX < 0 && facingRight) Flip();
     }
 
     void Flip()
@@ -39,6 +36,15 @@ public class PlayerMovement : MonoBehaviour
         facingRight = !facingRight;
         Vector3 scale = transform.localScale;
         scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    IEnumerator WaitFlip()
+    {
+        yield return new WaitForSeconds(12f);
+        facingRight = true;
+        Vector3 scale = transform.localScale;
+        scale.x = Mathf.Abs(scale.x);
         transform.localScale = scale;
     }
 }
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,26 +2,45 @@ using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{    
+{
+    public EthanCraneBasics playerMovement;
+    public Rigidbody2D playerRb;
+    public Transform masa;
+    public float mesafe = 1f;
+
     void Start()
     {
         Debug.Log("[SÝSTEM]: Tiz Ses Seviyesi %100. Ethan Uyanýyor.");
-    }
-
-    void Update()
-    {
-        
+        playerRb.simulated = false;
+        playerMovement.canMove = false;
+        StartCoroutine(ControlTutorial());
     }
 
     IEnumerator ControlTutorial()
     {
-        yield return new WaitForSeconds(6f);
+        yield return new WaitForSeconds(3f);
         Debug.Log("(W) Ýleri");
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(3f);
         Debug.Log("(A) Sol");
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(3f);
         Debug.Log("(S) Geri");
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(3f);
         Debug.Log("(D) Sað");
+
+        playerRb.simulated = true;
+        playerMovement.canMove = true;
+        Debug.Log("Þimdi A/D ile masaya git!");
+
+        yield return new WaitUntil(() =>
+            Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)
+        );
+
+        Debug.Log("Masaya doðru ilerle...");
+
+        yield return new WaitUntil(() =>
+            Vector2.Distance(playerRb.position, masa.position) < mesafe
+        );
+
+        Debug.Log("Masaya ulaþtýn! Tutorial bitti.");
     }
 }
